a9585735f1381b0cdabf71837a323d59f96c249e
James Harned <jharned@purdue.edu>
2014-09-02 18:13:02 -0400

    Finished assignment

--- PA01/answer01.c
+++ PA01/answer01.c
@@ -3,25 +3,75 @@
 
 int arraySum(int * array, int len)
 {
-    return 0;
+    int summation = 0;
+    int i = 0;
+    for(i = 0; i < len; ++i){
+	summation = summation + array[i];
+	}
+    return (summation);
 }
 
 int arrayCountNegative(int * array, int len)
 {
-    return 0;
+    int negcount = 0;
+    int i = 0; 
+    for(i = 0; i < len; ++i){
+	if(array[i] < 0){
+	    ++negcount;
+	}
+    } 
+    return (negcount);
 }
 
 int arrayIsIncreasing(int * array, int len)
 {    
-    return 0;
+    int isincrease = 1;    
+    int i = 0;
+
+    for(i = 0; i < (len - 1); ++i){
+	if(array[i+1] < array[i]){
+	    isincrease = 0;
+	}
+    }    
+
+    if(len == 0){
+	isincrease = 1;
+    }
+ 	        
+    return (isincrease);
 }
 
 int arrayIndexRFind(int needle, const int * haystack, int len)
 {
-    return 0;
+    int position = -1;
+    int i = 0;
+
+    for(i = 0; i < len; ++i){
+	if(haystack[i] == needle){
+	    position = i;
+	}
+    }
+
+    return (position);
 }
 
 int arrayFindSmallest(int * array, int len)
 {
-    return 0;
+    int smallest = array[0];
+    int i = 0;
+    int smallpos = 0;
+
+    for(i = 0; i < len; ++i){
+	if(array[i] < smallest){
+	    smallest = array[i];
+	}
+    }
+
+    for(i = (len-1); i >= 0; --i){
+	if(array[i] == smallest){
+	   smallpos = i;
+	}
+    }
+
+    return (smallpos);
 }
